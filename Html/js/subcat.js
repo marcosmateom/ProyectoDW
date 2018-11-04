2b475d6367ca4d3c489e5cde142eb6c9fb647336
Miguel González <miguelgon_8@hotmail.com>
2018-11-03 21:23:06 -0600

    quebrarse descripcion semantica y arreglar .php a .html de todo

--- Html/js/subcat.js
+++ Html/js/subcat.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
             for (var e = 0; e < data['subcat'].length; e++) {
                 if (1 == data['subcat'][e][1]) {
                     $subc2.append('<div class="cell">' +
-                        '<a href="view_p.php?subcat=' + data['subcat'][e][0] + '">' +
+                        '<a href="view_p.html?subcat=' + data['subcat'][e][0] + '">' +
                         '<div class="card">' +
                         '<img src="https://placehold.it/180x180">' +
                         '<div class="card-section">' +
